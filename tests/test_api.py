5919870e11d81890628125a4af62721d39d16056
zoubir <zoubir.ajaada@hotmail.fr>
2026-04-28 20:34:21 +0200

    fix: Ajout de l'autorisation pour l'appel de l'api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,15 @@
 import pytest
+import os
 
 # ======== /predict_nouveau (données brutes) ========
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer {0}'.format(os.getenv("API_KEY"))
+}
 
 def test_predict_valide(client, employe_valide):
     """Un employé valide doit retourner 200 avec une prédiction"""
-    response = client.post("/predict_nouveau", json=employe_valide)
+    response = client.post("/predict_nouveau", json=employe_valide, headers=headers)
 
     assert response.status_code == 200
     data = response.json()
@@ -15,39 +20,40 @@ def test_predict_valide(client, employe_valide):
 
 def test_predict_champ_manquant(client):
     """Un champ obligatoire manquant doit retourner 422"""
-    response = client.post("/predict_nouveau", json={"age": 28})  # plein de champs manquants
+
+    response = client.post("/predict_nouveau", json={"age": 28}, headers=headers)  # plein de champs manquants
     assert response.status_code == 422
 
 def test_predict_poste_invalide(client, employe_valide):
     """Un poste inconnu doit retourner une erreur de validation"""
     employe_valide["poste"] = "astronaute"  # poste qui n'existe pas
-    response = client.post("/predict_nouveau", json=employe_valide)
+    response = client.post("/predict_nouveau", json=employe_valide, headers=headers)
     assert response.status_code == 422
 
 def test_predict_frequence_invalide(client, employe_valide):
     """Une fréquence invalide doit retourner 422"""
     employe_valide["frequence_deplacement"] = "parfois"
-    response = client.post("/predict_nouveau", json=employe_valide)
+    response = client.post("/predict_nouveau", json=employe_valide, headers=headers)
     assert response.status_code == 422
 
 def test_predict_age_hors_limites(client, employe_valide):
     """Un age impossible doit retourner 422"""
     employe_valide["age"] = 150
-    response = client.post("/predict_nouveau", json=employe_valide)
+    response = client.post("/predict_nouveau", json=employe_valide, headers=headers)
     assert response.status_code == 422
 
 # ======== /predict/{id} (depuis BDD) ========
 
 def test_predict_by_id_valide(client):
     """Un id existant doit retourner une prédiction"""
-    response = client.post("/predict/1")
+    response = client.post("/predict/1", headers=headers)
     assert response.status_code == 200
     data = response.json()
     assert "prediction" in data
 
 def test_predict_by_id_inexistant(client):
     """Un id qui n'existe pas doit retourner une erreur claire"""
-    response = client.post("/predict/999999")
+    response = client.post("/predict/999999", headers=headers)
     assert response.status_code == 200  # ton endpoint retourne 200 avec un message d'erreur
     data = response.json()
     assert "Erreur" in data
