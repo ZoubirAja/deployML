40df9fecbddc4a4eaf453ac14746aac796f09e45
zoubir <zoubir.ajaada@hotmail.fr>
2026-04-29 02:27:20 +0200

    config: Configuration pour utilisation dans hugging face

--- database.py
+++ database.py
@@ -5,6 +5,11 @@ import os
 load_dotenv()
 
 def get_connection():
+    # CAS D'UTILISATION DANS HUGGING FACE
+    database_url = os.getenv("DATABASE_URL")
+    if database_url:
+        return psycopg2.connect(database_url)
+    
     return psycopg2.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
