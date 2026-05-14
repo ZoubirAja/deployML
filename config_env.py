5fe989dafa637796a8423e282257c9f71c1ce529
zoubir <zoubir.ajaada@hotmail.fr>
2026-05-14 11:39:54 +0200

    test: On entre le fichier en dur dans le config

--- config_env.py
+++ config_env.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 APP_ENV = os.getenv("APP_ENV", "dev")
 
-env_file = f".env.{APP_ENV}"
+env_file = f".env.test"
 if os.path.exists(env_file):
     load_dotenv(env_file)
 else:
-    load_dotenv(".env")  # fallback
+    load_dotenv(".env.test")  # fallback
 
 DEBUG = os.getenv("DEBUG", "false").lower() == "true"
