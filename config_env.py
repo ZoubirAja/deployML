8899735aa03560a3a27f260ee581099b3db72441
zoubir <zoubir.ajaada@hotmail.fr>
2026-05-14 14:28:28 +0200

    test: reset pour tester

--- config_env.py
+++ config_env.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 APP_ENV = os.getenv("APP_ENV", "dev")
 
-env_file = f".env.test"
+env_file = f".env.{APP_ENV}"
 if os.path.exists(env_file):
     load_dotenv(env_file)
 else:
-    load_dotenv(".env.test")  # fallback
+    load_dotenv(".env")  # fallback
 
 DEBUG = os.getenv("DEBUG", "false").lower() == "true"
